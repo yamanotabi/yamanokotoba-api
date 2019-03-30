101d31068a4ac096e92e06eddbb254e7ef2e381c
shiki <shiki.tak@gmail.com>
2019-03-30 16:41:43 +0900

    Overlay the text on the image and upload to S3

---
+++ app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,66 @@
+module Api
+    module V1
+        class Api::V1::ImagesController < ApplicationController
+            require 'mini_magick'
+
+            GRAVITY = 'center'.freeze
+            TEXT_POSITION = '0,0'.freeze
+            FONT = 'app/assets/fonts/komorebi-gothic.ttf'.freeze
+            FONT_SIZE = 45
+            INDENTION_COUNT = 15
+
+            # POST /api/v1/images
+            def create
+                s3 = AWS::S3.new(
+                    :access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
+                    :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+                    :region => 'ap-northeast-1')
+                bucket = s3.buckets[ENV["S3_BUCKET"]]
+                @data = params[:file]
+                @image = MiniMagick::Image.read(params[:file])
+                @image.resize "900x600"
+                
+                file_name = @data.original_filename
+                file_full_path="images/"+file_name
+
+                build("ソードアートオンラインではシノン推し")
+
+                post = ActionDispatch::Http::UploadedFile.new(
+                    filename:file_name,
+                    type: 'image/png',
+                    tempfile: @image.tempfile
+                )
+
+                object = bucket.objects[file_full_path]
+                object.write(post.open, :acl => :public_read)
+
+                # "http://s3-ap-northeast-1.amazonaws.com/images/#{file_name}"
+                render status: 200, json: { status: 200, image: post }
+            end
+
+            # 合成後のFileClassを生成
+            def build(text)
+                text = prepare_text(text)
+                configuration(text)
+            end
+    
+            private    
+            # 設定関連の値を代入
+            def configuration(text)
+                @image.combine_options do |config|
+                config.font FONT
+                config.gravity GRAVITY
+                config.pointsize FONT_SIZE
+                config.fill("#FFFFFF") 
+                config.draw "text #{TEXT_POSITION} '#{text}'"
+                end
+            end
+    
+            # 背景にいい感じに収まるように文字を調整して返却
+            def prepare_text(text)
+                text.scan(/.{1,#{INDENTION_COUNT}}/)[0...(text.length / INDENTION_COUNT + 1)].join("\n")
+            end
+
+        end
+    end
+end
