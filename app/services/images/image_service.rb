9880ca9cac5ff83dbbf574d43bfb26d84c906ebc
shiki <shiki.tak@gmail.com>
2019-04-02 01:09:03 +0900

    bug fix typo

--- app/services/images/image_service.rb
+++ app/services/images/image_service.rb
@@ -9,7 +9,7 @@ module Images
         INDENTION_COUNT = 15
 
         class << self
-            def upload(data, test)
+            def upload(data, text)
                 s3 = AWS::S3.new(
                     :access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
                     :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
